560505cbab2b4ff0ec094c0ca84fcbdcbfa3d0d1
Tryanda Anggita Suwito <tryandaasu@gmail.com>
2024-12-28 17:02:59 +0700

    add-fungsi-delete

--- hapus_mobil.php
+++ hapus_mobil.php
@@ -1,12 +1,21 @@
 <?php
 include 'koneksi.php';
 
-$id = $_GET['id'];
-$query = "DELETE FROM tbl_mobil_tryandaasu WHERE no_plat_tryandaasu = '$id'";
-
-if ($conn->query($query)) {
-    header('Location: mobil.php');
+if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+    
+    $query_rental = "DELETE FROM tbl_rental_tryandaasu WHERE no_plat_tryandaasu = '$id'";
+    $conn->query($query_rental);
+    
+    $query_mobil = "DELETE FROM tbl_mobil_tryandaasu WHERE no_plat_tryandaasu = '$id'";
+    
+    if ($conn->query($query_mobil)) {
+        header('Location: mobil.php');
+        exit;
+    } else {
+        echo "Error: " . $conn->error;
+    }
 } else {
-    echo "Error: " . $conn->error;
+    echo "ID tidak ditemukan";
 }
 ?>
